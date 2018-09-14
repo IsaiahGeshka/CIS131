fed8e41401e38828f0bb7764901caec009eee900
IsaiahGeshka <42741552+IsaiahGeshka@users.noreply.github.com>
2018-09-14 15:44:13 -0500

    Created HWArray.js

---
+++ HomeWork Assignments/HWArray.js
@@ -0,0 +1,20 @@
+//Musketeers Array
+const musketeers = ["Athos", "Porthos", "Aramin"]
+
+const elements = ["Mishka", 7, {message: "Hazit man"}, true];
+console.log(musketeers.length);
+console.log(musketeers[1]);
+console.log();
+
+for (n = 0; n < musketeers.length; n++){
+    console.log(musketeers[n]);
+}
+
+
+
+
+// Sum of values
+const values = [3, 11, 7, 2, 9, 10];
+
+//Array maximum
+// const values = [3, 11, 7, 2, 9, 10]
