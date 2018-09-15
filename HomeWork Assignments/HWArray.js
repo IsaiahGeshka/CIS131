d7fd640ca63af6fbd6180ddb50a76d5b520bbcd0
IsaiahGeshka <42741552+IsaiahGeshka@users.noreply.github.com>
2018-09-14 19:56:27 -0500

    alomost done with array hw

--- HomeWork Assignments/HWArray.js
+++ HomeWork Assignments/HWArray.js
@@ -1,20 +1,45 @@
 //Musketeers Array
-const musketeers = ["Athos", "Porthos", "Aramin"]
+const musketeers = ["Athos", "Porthos", "Aramis"]
 
 const elements = ["Mishka", 7, {message: "Hazit man"}, true];
 console.log(musketeers.length);
-console.log(musketeers[1]);
+console.log(musketeers[1,2]);
 console.log();
 
 for (n = 0; n < musketeers.length; n++){
     console.log(musketeers[n]);
 }
+//Add D'Artagnan to end of array
+musketeers.push("D'Artagnan");
 
+//For Each()
 
+musketeers.forEach(myElement => {
+    console.log(`For Loop ${myElement}`);
+})
+
+//POP
+musketeers.pop();
+let theEndElment = musketeers.pop();
+
+//For-Of 
+let ofMusketeers;
+for (ofMusketeers of musketeers){
+    console.log(ofMusketeers);
+}
+
+console.log(musketeers)
 
 
 // Sum of values
-const values = [3, 11, 7, 2, 9, 10];
+const values = [3, 11, 7, 2, 9, 10]; 
+
 
 //Array maximum
-// const values = [3, 11, 7, 2, 9, 10]
+console.log([5])
+
+var sum = 0;
+for(var i = 0; i < values.length; i++){
+    sum += values[i]
+}
+console.log("The sum of values is: " +sum);
